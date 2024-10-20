e8e2454f86512191d9212105df43bbb2885cb701
Arin DeLoatch <81663999+arin-deloatch@users.noreply.github.com>
2024-10-20 22:42:56 +0000

    model config files

--- src/app.py
+++ src/app.py
@@ -1,24 +1,18 @@
 import streamlit as st
 import torch
-from transformers import DistilBertTokenizer, DistilBertForQuestionAnswering
+from transformers import BertTokenizer, BertForQuestionAnswering
 from transformers import pipeline
 
 
 class QACompanion:
     def __init__(self, MODEL_PATH: str, TOKENIZER_PATH: str) -> None:
         self.title = 'QA Companion 🤖'
-        self.model = DistilBertForQuestionAnswering.from_pretrained(MODEL_PATH)
-        self.tokenizer = DistilBertTokenizer.from_pretrained(TOKENIZER_PATH)
-      
-        
-    def generate_answer(self, question: str, context:str):
-        try:
-            question_answerer = pipeline("question-answering", model=self.model,
+        self.model = BertForQuestionAnswering.from_pretrained(MODEL_PATH)
+        self.tokenizer = BertTokenizer.from_pretrained(TOKENIZER_PATH)
+
+        self.qa = pipeline("question-answering", model=self.model,
                                          tokenizer=self.tokenizer)
-            question_answerer(question=question, context=context)
-        except Exception as e:
-            return f"Error generating answer: {e}"
-    
+        
     def app(self) -> None:
         st.title(self.title)
 
@@ -28,7 +22,7 @@ class QACompanion:
         if st.button("Generate Answer"):
             if question:
                 with st.spinner("Generating answer..."):
-                    answer = self.generate_answer(question,context)
+                    answer = self.qa(question,context)
                     st.write("### Answer:")
                     st.write(answer)
             else:
@@ -39,7 +33,7 @@ if __name__ == '__main__':
     '''
     TODO: Replace the MODEL_PATH AND TOKENIZER_PATH with our fine-tuned model
     '''
-    MODEL_PATH = 'distilbert-base-uncased'
-    TOKENIZER_PATH = 'distilbert-base-uncased'
+    MODEL_PATH = 'google-bert/bert-base-uncased'
+    TOKENIZER_PATH = 'google-bert/bert-base-uncased'
     bot = QACompanion(MODEL_PATH=MODEL_PATH, TOKENIZER_PATH=TOKENIZER_PATH)
     bot.app()
